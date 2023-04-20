835ae1e94c6cae4cbc6c9f3da426288be2620b29
thiagosan593 <thiago_593@hotmail.com>
2023-04-20 20:16:56 -0300

    correção

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-function mudouTamanho(){
- if(window.innerWidth >=1000){
-    itens.style.display ='block'
-     }else{
-    itens.style.display='none'
- }
- }
-function clickMenu(){
-          if(  itens.style.display =='block'){
-            itens.style.display ='none'
-          }else{
-            itens.style.display ='block'
-  }
- }
-    
 
  window.addEventListener("load", function() {
 
@@ -29,3 +14,19 @@ setTimeout(function() {
 
 });
 
+function mudouTamanho(){
+  if(window.innerWidth >=1000){
+     itens.style.display ='block'
+      }else{
+     itens.style.display='none'
+  }
+  }
+ function clickMenu(){
+           if(  itens.style.display =='block'){
+             itens.style.display ='none'
+           }else{
+             itens.style.display ='block'
+   }
+  }
+     
+ 
